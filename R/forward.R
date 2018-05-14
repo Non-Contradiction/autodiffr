6294c993b1f37a61fa734747914018efbf170de8
Non-Contradiction <lch34677@gmail.com>
2018-05-14 19:20:15 -0400

    The initial simple wrappers of derivative, gradient, jacobian and hessian in ForwardDiff.

---
+++ R/forward.R
@@ -0,0 +1,21 @@
+forward.deriv <- function(f, x){
+    JuliaCall::julia_call("ForwardDiff.derivative", f, x)
+}
+
+forward.grad <- function(f, x,
+                         cfg = JuliaCall::julia_call("GradientConfig", f, x),
+                         check = JuliaCall::julia_call("Val{true}")){
+    JuliaCall::julia_call("ForwardDiff.gradient", f, x, cfg, check)
+}
+
+forward.jacobian <- function(f, x,
+                             cfg = JuliaCall::julia_call("JacobianConfig", f, x),
+                             check = JuliaCall::julia_call("Val{true}")){
+    JuliaCall::julia_call("ForwardDiff.jacobian", f, x, cfg, check)
+}
+
+forward.hessian <- function(f, x,
+                            cfg = JuliaCall::julia_call("HessianConfig", f, x),
+                            check = JuliaCall::julia_call("Val{true}")){
+    JuliaCall::julia_call("ForwardDiff.hessian", f, x, cfg, check)
+}
