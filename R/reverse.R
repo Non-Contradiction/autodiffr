5886458709e7f16e9e975ed226dc3ca89f2a78d7
Non-Contradiction <lch34677@gmail.com>
2018-05-15 12:27:25 -0400

    Exportation and documentation for wrapper functions of API of ReverseDiff.

--- R/reverse.R
+++ R/reverse.R
@@ -6,34 +6,26 @@
 #'  for your functions using reverse mode automatic differentiation.
 #'  For more details, see <http://www.juliadiff.org/ReverseDiff.jl/api/>.
 #'
-#' @param f the function you want to calulate the derivative, gradient and etc.
+#' @param f the function you want to calulate the gradient, jacobian and hessian.
 #'   Note that `f(x)` should be a scalar for `grad` and `hessian`,
-#'   a vector of length greater than 1 for `jacobian`,
-#'   and could be either a scalar or a vector for `deriv`.
-#' @param x the point where you take the derivative, gradient and etc.
-#'   Note that it should be a scalar for `deriv` and a vector of length greater than 1 for
-#'   `grad`, `jacobian` and `hessian`.
-#' @param cfg objects of ForwardDiff.AbstractConfig types, which have
-#'   information useful to do automatic differentiation for `f`.
-#'   These types allow the user to easily feed several different parameters to ForwardDiff's API,
-#'   such as chunk size, work buffers, and perturbation seed configurations.
-#'   ForwardDiff's basic API methods will allocate these types automatically by default,
-#'   but you can drastically reduce memory usage if you preallocate them yourself.
-#' @param check whether to allow tag checking.
-#'   Set check to `JuliaCall::julia_call("Val{false}")` to disable tag checking for ForwardDiff.
-#'   This can lead to perturbation confusion, so should be used with care.
-#' @param chunk the chunk to construct the Config objects for ForwardDiff.
-#'   Its size may be explicitly provided, or omitted,
-#'   in which case ForwardDiff will automatically select a chunk size for you.
-#'   However, it is highly recommended to specify the chunk size manually when possible.
+#'   a vector of length greater than 1 for `jacobian`.
+#' @param input the point where you take the gradient, jacobian and hessian.
+#'   Note that it should be a a vector of length greater than 1.
+#'   If you want to calulate the derivative of a function, you can considering using `forward.deriv`.
+#' @param cfg Config objects which contains the preallocated tape and work buffers
+#'   used by reverse mode automatic differentiation.
+#'   `ReverseDiff`'s API methods will allocate the Config object automatically by default,
+#'   but you can preallocate them yourself and reuse them for subsequent calls to reeuce memory usage.
+#' @param tp,gtp,jtp the tapes to construct the Config objects for `ReverseDiff`,
+#'   which are for advantage usage.
 #'   See
-#'   <http://www.juliadiff.org/ForwardDiff.jl/stable/user/advanced.html#Configuring-Chunk-Size-1>
+#'   <http://www.juliadiff.org/ReverseDiff.jl/api/#the-abstracttape-api>
 #'   for more details.
-#' @return `forward.deriv`, `forward.grad`, `forward.jacobian` and `forward.hessian` return
-#'   the derivative, gradient, jacobian and hessian of `f` correspondingly evaluated at `x`.
-#'   `forward.grad.config`, `forward.jacobian.config` and `forward.hessian.config`
-#'   return Config instances based on `f`` and `x`,
-#'   which contain all the work buffers required to carry out the automatic differentiation.
+#' @return `reverse.grad`, `reverse.jacobian` and `reverse.hessian` return
+#'   the gradient, jacobian and hessian of `f` correspondingly evaluated at `input`.
+#'   `reverse.grad.config`, `reverse.jacobian.config` and `reverse.hessian.config`
+#'   return Config instances containing the preallocated tape and work buffers used by
+#'   reverse mode automatic differentiation.
 #' @md
 #'
 #' @name ReverseDiff
