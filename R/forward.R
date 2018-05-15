777be4b0c2dbaaf877a63802440ac24fb3740f4a
Non-Contradiction <lch34677@gmail.com>
2018-05-15 14:31:16 -0400

    Need to call the specific function in ForwardDiff for API of ForwardDiff.

--- R/forward.R
+++ R/forward.R
@@ -48,7 +48,7 @@ forward.deriv <- function(f, x){
 #' @rdname ForwardDiff
 #' @export
 forward.grad <- function(f, x,
-                         cfg = JuliaCall::julia_call("GradientConfig", f, x),
+                         cfg = JuliaCall::julia_call("ForwardDiff.GradientConfig", f, x),
                          check = JuliaCall::julia_call("Val{true}")){
     JuliaCall::julia_call("ForwardDiff.gradient", f, x, cfg, check)
 }
@@ -56,7 +56,7 @@ forward.grad <- function(f, x,
 #' @rdname ForwardDiff
 #' @export
 forward.jacobian <- function(f, x,
-                             cfg = JuliaCall::julia_call("JacobianConfig", f, x),
+                             cfg = JuliaCall::julia_call("ForwardDiff.JacobianConfig", f, x),
                              check = JuliaCall::julia_call("Val{true}")){
     JuliaCall::julia_call("ForwardDiff.jacobian", f, x, cfg, check)
 }
@@ -64,7 +64,7 @@ forward.jacobian <- function(f, x,
 #' @rdname ForwardDiff
 #' @export
 forward.hessian <- function(f, x,
-                            cfg = JuliaCall::julia_call("HessianConfig", f, x),
+                            cfg = JuliaCall::julia_call("ForwardDiff.HessianConfig", f, x),
                             check = JuliaCall::julia_call("Val{true}")){
     JuliaCall::julia_call("ForwardDiff.hessian", f, x, cfg, check)
 }
@@ -74,20 +74,20 @@ forward.hessian <- function(f, x,
 #' @rdname ForwardDiff
 #' @export
 forward.grad.config <- function(f, x,
-                                chunk = JuliaCall::julia_call("Chunk", x)){
+                                chunk = JuliaCall::julia_call("ForwardDiff.Chunk", x)){
     JuliaCall::julia_call("ForwardDiff.GradientConfig", f, x, chunk)
 }
 
 #' @rdname ForwardDiff
 #' @export
 forward.jacobian.config <- function(f, x,
-                                    chunk = JuliaCall::julia_call("Chunk", x)){
+                                    chunk = JuliaCall::julia_call("ForwardDiff.Chunk", x)){
     JuliaCall::julia_call("ForwardDiff.JacobianConfig", f, x, chunk)
 }
 
 #' @rdname ForwardDiff
 #' @export
 forward.hessian.config <- function(f, x,
-                                   chunk = JuliaCall::julia_call("Chunk", x)){
+                                   chunk = JuliaCall::julia_call("ForwardDiff.Chunk", x)){
     JuliaCall::julia_call("ForwardDiff.HessianConfig", f, x, chunk)
 }
