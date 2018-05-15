f04ac7ea78fa5ba105d5eda667862ff6d94a0eea
Non-Contradiction <lch34677@gmail.com>
2018-05-15 11:12:32 -0400

    Nearly finish the exportation and documentation of ForwardDiff's API.

--- R/forward.R
+++ R/forward.R
@@ -1,5 +1,11 @@
 #' Wrapper functions for API of `ForwardDiff.jl`.
 #'
+#'  Wrapper functions for API of `ForwardDiff.jl` at
+#'  <http://www.juliadiff.org/ForwardDiff.jl/stable/user/api.html>.
+#'  These functions can help you calculate derivative, gradient, jacobian and hessian
+#'  for your functions using forward mode automatic differentiation.
+#'  For more details, see <http://www.juliadiff.org/ForwardDiff.jl/stable/user/api.html>.
+#'
 #' @param f the function you want to calulate the derivative, gradient and etc.
 #'   Note that `f(x)` should be a scalar for `grad` and `hessian`,
 #'   a vector of length greater than 1 for `jacobian`,
@@ -7,11 +13,27 @@
 #' @param x the point where you take the derivative, gradient and etc.
 #'   Note that it should be a scalar for `deriv` and a vector of length greater than 1 for
 #'   `grad`, `jacobian` and `hessian`.
-#' @param cfg
-#' @param check
-#' @param chunk
-#' @return julia_installed_package will return the version number of the julia package,
-#'     "nothing" if the package is not installed.
+#' @param cfg objects of ForwardDiff.AbstractConfig types, which have
+#'   information useful to do automatic differentiation for `f`.
+#'   These types allow the user to easily feed several different parameters to ForwardDiff's API,
+#'   such as chunk size, work buffers, and perturbation seed configurations.
+#'   ForwardDiff's basic API methods will allocate these types automatically by default,
+#'   but you can drastically reduce memory usage if you preallocate them yourself.
+#' @param check whether to allow tag checking.
+#'   Set check to `JuliaCall::julia_call("Val{false}")` to disable tag checking for ForwardDiff.
+#'   This can lead to perturbation confusion, so should be used with care.
+#' @param chunk the chunk to construct the Config objects for ForwardDiff.
+#'   Its size may be explicitly provided, or omitted,
+#'   in which case ForwardDiff will automatically select a chunk size for you.
+#'   However, it is highly recommended to specify the chunk size manually when possible.
+#'   See
+#'   <http://www.juliadiff.org/ForwardDiff.jl/stable/user/advanced.html#Configuring-Chunk-Size-1>
+#'   for more details.
+#' @return `forward.deriv`, `forward.grad`, `forward.jacobian` and `forward.hessian` return
+#'   the derivative, gradient, jacobian and hessian of `f` correspondingly evaluated at `x`.
+#'   `forward.grad.config`, `forward.jacobian.config` and `forward.hessian.config`
+#'   return Config instances based on `f`` and `x`,
+#'   which contain all the work buffers required to carry out the automatic differentiation.
 #'
 #' @name ForwardDiff
 NULL
