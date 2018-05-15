f9e3854bfa2b2d542443b07e433d75215e9b8dbc
Non-Contradiction <lch34677@gmail.com>
2018-05-14 23:11:49 -0400

    Finish all the simple implementations of wrappers of API of ReverseDiff. Need to have more documentation and tests.

--- R/reverse.R
+++ R/reverse.R
@@ -12,3 +12,21 @@ reverse.hessian <- function(f, input,
                             cfg = JuliaCall::julia_call("HessianConfig", input)){
     JuliaCall::julia_call("ReverseDiff.hessian", f, input, cfg)
 }
+
+## Constructing Config objects for ReverseDiff
+
+reverse.grad.config <- function(input,
+                                tp = JuliaCall::julia_call("Rawtape")){
+    JuliaCall::julia_call("ReverseDiff.GradientConfig", input, tp)
+}
+
+reverse.jacobian.config <- function(input,
+                                    tp = JuliaCall::julia_call("Rawtape")){
+    JuliaCall::julia_call("ReverseDiff.JacobianConfig", input, tp)
+}
+
+reverse.hessian.config <- function(input,
+                                   gtp = JuliaCall::julia_call("Rawtape"),
+                                   jtp = JuliaCall::julia_call("Rawtape")){
+    JuliaCall::julia_call("ReverseDiff.HessianConfig", input, gtp, jtp)
+}
