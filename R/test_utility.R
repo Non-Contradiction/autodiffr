5b83160233923f870159c82d53b22fed76787cbf
Non-Contradiction <lch34677@gmail.com>
2018-05-16 13:59:11 -0400

    Add some testing utility functions. Related to #5.

---
+++ R/test_utility.R
@@ -0,0 +1,39 @@
+expect_deriv <- function(f, x, result = NULL){
+    testthat::expect_identical(forward.deriv(f, x), result)
+}
+
+expect_grad <- function(f, x, result = NULL){
+    testthat::expect_s3_class(forward.grad.config(f, x), "JuliaObject")
+
+    testthat::expect_s3_class(reverse.grad.config(x), "JuliaObject")
+
+    testthat::expect_identical(forward.grad(f, x), reverse.grad(f, x))
+
+    if (!is.null(result)) {
+        testthat::expect_identical(forward.grad(f, x), result)
+    }
+}
+
+expect_hessian <- function(f, x, result = NULL){
+    testthat::expect_s3_class(forward.hessian.config(f, x), "JuliaObject")
+
+    testthat::expect_s3_class(reverse.hessian.config(x), "JuliaObject")
+
+    testthat::expect_identical(forward.hessian(f, x), reverse.hessian(f, x))
+
+    if (!is.null(result)) {
+        testthat::expect_identical(forward.hessian(f, x), result)
+    }
+}
+
+expect_jacobian <- function(f, x, result = NULL){
+    testthat::expect_s3_class(forward.jacobian.config(f, x), "JuliaObject")
+
+    testthat::expect_s3_class(reverse.jacobian.config(x), "JuliaObject")
+
+    testthat::expect_identical(forward.jacobian(f, x), reverse.jacobian(f, x))
+
+    if (!is.null(result)) {
+        testthat::expect_identical(forward.jacobian(f, x), result)
+    }
+}
