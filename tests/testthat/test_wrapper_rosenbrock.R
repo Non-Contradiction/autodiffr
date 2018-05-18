6714344ac6af5b11567734ed9f04ea517f34dcfc
Non-Contradiction <lch34677@gmail.com>
2018-05-18 15:04:05 -0400

    Update tests with the new utility with julia functions.

--- tests/testthat/test_wrapper_rosenbrock.R
+++ tests/testthat/test_wrapper_rosenbrock.R
@@ -11,11 +11,15 @@ test_that("Grad and Hessian for Rosenbrock function", {
         sum(100 * (x1 - x2^2)^2 + (1 - x2)^2)
     }
 
-    autodiffr:::expect_grad(fnRosenbrock, rep(1,3))
-    autodiffr:::expect_grad(fnRosenbrock, c(1,2,3))
-    autodiffr:::expect_grad(fnRosenbrock, c(1,2,1))
+    JuliaCall::julia_eval("function rosenjl(x) n = length(x);
+                          x1 = x[2:n]; x2 = x[1:(n - 1)];
+                          sum(100 * (x1 - x2.^2).^2 + (1 - x2).^2) end")
 
-    autodiffr:::expect_hessian(fnRosenbrock, rep(1,3))
-    autodiffr:::expect_hessian(fnRosenbrock, c(1,2,3))
-    autodiffr:::expect_hessian(fnRosenbrock, c(1,2,1))
+    autodiffr:::expect_grad(fnRosenbrock, rep(1,3), jf_str = "rosenjl")
+    autodiffr:::expect_grad(fnRosenbrock, c(1,2,3), jf_str = "rosenjl")
+    autodiffr:::expect_grad(fnRosenbrock, c(1,2,1), jf_str = "rosenjl")
+
+    autodiffr:::expect_hessian(fnRosenbrock, rep(1,3), jf_str = "rosenjl")
+    autodiffr:::expect_hessian(fnRosenbrock, c(1,2,3), jf_str = "rosenjl")
+    autodiffr:::expect_hessian(fnRosenbrock, c(1,2,1), jf_str = "rosenjl")
 })
