46d95dae9b8d2d0ae93f6750476292644b9422f7
Non-Contradiction <lch34677@gmail.com>
2018-05-18 14:57:30 -0400

    Add utilities to make sure the R result is matched with the Julia version.

--- R/test_utility.R
+++ R/test_utility.R
@@ -1,8 +1,15 @@
-expect_deriv <- function(f, x, result){
+expect_deriv <- function(f, x, result, jf_str = NULL){
     testthat::expect_equal(forward.deriv(f, x), result)
+
+    if (!is.null(jf_str)) {
+        JuliaCall::julia_assign("x", x)
+        command <- paste0("ForwardDiff.derivative(", jf_str, "x)")
+        testthat::expect_equal(forward.deriv(f, x),
+                               JuliaCall::julia_eval(command))
+    }
 }
 
-expect_grad <- function(f, x, result = NULL){
+expect_grad <- function(f, x, result = NULL, julia_version = NULL, jf_str = NULL){
     testthat::expect_s3_class(forward.grad.config(f, x), "JuliaObject")
 
     testthat::expect_s3_class(reverse.grad.config(x), "JuliaObject")
@@ -12,9 +19,16 @@ expect_grad <- function(f, x, result = NULL){
     if (!is.null(result)) {
         testthat::expect_equal(forward.grad(f, x), result)
     }
+
+    if (!is.null(jf_str)) {
+        JuliaCall::julia_assign("x", x)
+        command <- paste0("ForwardDiff.gradient(", jf_str, "x)")
+        testthat::expect_equal(forward.grad(f, x),
+                               JuliaCall::julia_eval(command))
+    }
 }
 
-expect_hessian <- function(f, x, result = NULL){
+expect_hessian <- function(f, x, result = NULL, jf_str = NULL){
     testthat::expect_s3_class(forward.hessian.config(f, x), "JuliaObject")
 
     testthat::expect_s3_class(reverse.hessian.config(x), "JuliaObject")
@@ -24,9 +38,16 @@ expect_hessian <- function(f, x, result = NULL){
     if (!is.null(result)) {
         testthat::expect_equal(forward.hessian(f, x), result)
     }
+
+    if (!is.null(jf_str)) {
+        JuliaCall::julia_assign("x", x)
+        command <- paste0("ForwardDiff.hessian(", jf_str, "x)")
+        testthat::expect_equal(forward.hessian(f, x),
+                               JuliaCall::julia_eval(command))
+    }
 }
 
-expect_jacobian <- function(f, x, result = NULL){
+expect_jacobian <- function(f, x, result = NULL, jf_str = NULL){
     testthat::expect_s3_class(forward.jacobian.config(f, x), "JuliaObject")
 
     testthat::expect_s3_class(reverse.jacobian.config(x), "JuliaObject")
@@ -35,5 +56,12 @@ expect_jacobian <- function(f, x, result = NULL){
 
     if (!is.null(result)) {
         testthat::expect_equal(forward.jacobian(f, x), result)
+    }
+
+    if (!is.null(jf_str)) {
+        JuliaCall::julia_assign("x", x)
+        command <- paste0("ForwardDiff.jacobian(", jf_str, "x)")
+        testthat::expect_equal(forward.jacobian(f, x),
+                               JuliaCall::julia_eval(command))
     }
 }
