786383206d01f39c08d4cbf66656e784f6b27d8b
Non-Contradiction <lch34677@gmail.com>
2018-05-19 12:21:25 -0400

    More utility functions from test utility function in ForwardDiff.

--- R/test_utility.R
+++ R/test_utility.R
@@ -72,4 +72,19 @@ test_setup <- function(){
     JuliaCall::julia_library("Calculus")
     JuliaCall::julia_install_package_if_needed("DiffTests")
     JuliaCall::julia_library("DiffTests")
+
+    ## Adapted from test utility functions of ForwardDiff at
+    ## <https://github.com/JuliaDiff/ForwardDiff.jl/blob/master/test/utils.jl>
+    eval.parent(quote({
+        ## convenient function in R corresponding to div in Julia
+        div <- function(x, y) as.integer(x / y)
+        set.seed(1)
+        DEFAULT_CHUNK_THRESHOLD <- JuliaCall::julia_eval("ForwardDiff.DEFAULT_CHUNK_THRESHOLD")
+        XLEN <- DEFAULT_CHUNK_THRESHOLD + 1
+        YLEN <- div(DEFAULT_CHUNK_THRESHOLD, 2) + 1
+        X <- runif(XLEN)
+        Y <- runif(YLEN)
+        CHUNK_SIZES <- c(1, div(DEFAULT_CHUNK_THRESHOLD, 3), div(DEFAULT_CHUNK_THRESHOLD, 2), DEFAULT_CHUNK_THRESHOLD, DEFAULT_CHUNK_THRESHOLD + 1)
+        FINITEDIFF_ERROR = 3e-5
+    }))
 }
