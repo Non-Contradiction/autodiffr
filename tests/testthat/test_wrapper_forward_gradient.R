3f443da60a13a745e68ef8c09c66f3b86e83f293
Non-Contradiction <lch34677@gmail.com>
2018-05-19 12:27:14 -0400

    More tests from tests in ForwardDiff.

--- tests/testthat/test_wrapper_forward_gradient.R
+++ tests/testthat/test_wrapper_forward_gradient.R
@@ -32,4 +32,39 @@ test_that("test on rosenbrock function", {
         }
     }
 
+    cfgx <- forward.grad.config(sin, x)
+    expect_error(forward.grad(f, x, cfg = cfgx))
+    expect_identical(forward.grad(f, x, cfg = cfgx, check = JuliaCall::julia_call("Val{false}")),
+                     forward.grad(f,x))
+
+})
+
+test_that("test on VECTOR_TO_NUMBER_FUNCS", {
+    skip_on_cran()
+    ad_setup()
+    autodiffr:::test_setup()
+
+    ########################
+    # test vs. Calculus.jl #
+    ########################
+
+    for (f in autodiffr:::VECTOR_TO_NUMBER_FUNCS) {
+        v <- f(X)
+        g <- forward.grad(f, X)
+        expect_equal(g, JuliaCall::julia_call("Calculus.derivative", f, X))
+
+        for (c in CHUNK_SIZES) {
+            for (tag in list(JuliaCall::julia_eval("nothing", need_return = "Julia"),
+                             JuliaCall::julia_call("ForwardDiff.Tag",
+                                                   f, JuliaCall::julia_eval("Float64")))) {
+                JuliaCall::julia_assign("c", c)
+                cfg <- JuliaCall::julia_call("ForwardDiff.GradientConfig",
+                                             f, x,
+                                             JuliaCall::julia_eval("ForwardDiff.Chunk{c}()"),
+                                             tag)
+
+                expect_equal(g, forward.grad(f, x, cfg))
+            }
+        }
+    }
 })
