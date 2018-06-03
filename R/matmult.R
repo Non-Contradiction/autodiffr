b29766d9f11a34d022bb8a014563a3d8606da9aa
Non-Contradiction <lch34677@gmail.com>
2018-06-03 15:51:17 -0400

    %m% instead of %x%.

--- R/matmult.R
+++ R/matmult.R
@@ -1,8 +1,10 @@
 #' Matrix Multiplication Compatable with JuliaObject
 #'
-#' `%x%` is matrix multiplication operator compatible with `JuliaObject`.
+#' `%m%` is matrix multiplication operator compatible with `JuliaObject`.
 #'
 #' @param x,y numeric or complex matrices or vectors.
 #'
+#' @md
+#'
 #' @export
-`%x%` <- function(x, y) JuliaCall::julia_call("*", x, y)
+`%m%` <- function(x, y) JuliaCall::julia_call("*", x, y)
