24fa84b92711142247aaa4e24a9e1a168942c8a4
Non-Contradiction <lch34677@gmail.com>
2018-06-03 20:38:57 -0400

    Implementation of cSums, cMeans, rSums, rMeans to replace colSums, colMeans, rowSums and rowMeans.

---
+++ R/colSums.R
@@ -0,0 +1,26 @@
+#' Row and Column Sums and Means Compatable with JuliaObject
+#'
+#' Calculate row and column sums and means compatible with `JuliaObject`.
+#'
+#' @param x `R` or `julia` numeric matrices or vectors.
+#'
+#' @md
+#'
+#' @name colSums
+NULL
+
+#' @rdname colSums
+#' @export
+rSums <- function(x) JuliaCall::julia_call("sum", x, 2L)
+
+#' @rdname colSums
+#' @export
+rMeans <- function(x) JuliaCall::julia_call("mean", x, 2L)
+
+#' @rdname colSums
+#' @export
+cSums <- function(x) JuliaCall::julia_call("sum", x, 1L)
+
+#' @rdname colSums
+#' @export
+cMeans <- function(x) JuliaCall::julia_call("mean", x, 1L)
