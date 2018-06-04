14c141245071206ecff8741afcaef871e04a5974
Non-Contradiction <lch34677@gmail.com>
2018-06-03 20:39:30 -0400

    Implementation of Jmatrix instead of matrix.

---
+++ R/matrix.R
@@ -0,0 +1,13 @@
+#' `matrix` Compatible with `JuliaObject`.
+#'
+#' `Jmatrix` creates a matrix from the given set of values.
+#'
+#' @param data the data vector used to create matrix.
+#' @param nrow number of rows of the matrix.
+#' @param ncol number of columns of the matrix.
+#'
+#' @export
+Jmatrix <- function(data, nrow, ncol){
+    JuliaCall::julia_call("reshape", JuliaCall::julia_call("copy", data),
+                          nrow, ncol)
+}
