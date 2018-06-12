fc97dfce6c1c9b1d49114ba5666c8ec8dfba7112
Non-Contradiction <lch34677@gmail.com>
2018-06-12 15:03:45 -0400

    Prepare interface functions.

---
+++ R/interface.R
@@ -0,0 +1,23 @@
+#' Calculate the gradient of a function.
+#'
+#' \code{grad} calculates the gradient of a function.
+#'
+#' @param func the function you want to caculate the gradient,
+#'   it should be a function with a scalar result.
+#' @param x the point where the gradient is calculated.
+#'   If not given (or NULL), return will be the gradient function.
+#'
+#' @export
+grad <- function(func, x = NULL){
+    ## ad_setup() is not necessary,
+    ## unless you want to pass some arguments to it.
+    ad_setup()
+
+    if (!is.null(x)) {
+        JuliaCall::julia_call("ForwardDiff.gradient", func, x)
+    }
+    else {
+        force(func)
+        function(x) JuliaCall::julia_call("ForwardDiff.gradient", func, x)
+    }
+}
