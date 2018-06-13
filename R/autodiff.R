4bc7202d02e235b67db48088cbad410b6a15c527
Non-Contradiction <lch34677@gmail.com>
2018-06-13 15:15:01 -0400

    @eval seems to break the precompilation and causes problems with world age and some internal issue of Julia....

--- R/autodiff.R
+++ R/autodiff.R
@@ -20,12 +20,15 @@ ad_setup <- function(...) {
         .AD$julia$library("ForwardDiff")
         .AD$julia$library("ReverseDiff")
 
-        JuliaCall::julia_command("for R in (:Bool, :BigFloat)
-                                    @eval begin
-                                        Base.promote_rule(::Type{$R}, ::Type{ForwardDiff.Dual{T,V,N}}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type($R, V),N}
-                                        Base.promote_rule(::Type{ForwardDiff.Dual{T,V,N}}, ::Type{$R}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type(V, $R),N}
-                                    end
-                                 end")
+        # JuliaCall::julia_command("for R in (:Bool, :BigFloat)
+        #                             @eval begin
+        #                                 Base.promote_rule(::Type{$R}, ::Type{ForwardDiff.Dual{T,V,N}}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type($R, V),N}
+        #                                 Base.promote_rule(::Type{ForwardDiff.Dual{T,V,N}}, ::Type{$R}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type(V, $R),N}
+        #                             end
+        #                          end")
+        JuliaCall::julia_command("import Base.promote_rule")
+        JuliaCall::julia_command("Base.promote_rule(::Type{Bool}, ::Type{ForwardDiff.Dual{T,V,N}}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type(Bool, V),N}")
+        JuliaCall::julia_command("Base.promote_rule(::Type{ForwardDiff.Dual{T,V,N}}, ::Type{Bool}) where {T,V<:Real,N} = ForwardDiff.Dual{T,promote_type(V, Bool),N}")
 
         .AD$inited <- TRUE
     }
