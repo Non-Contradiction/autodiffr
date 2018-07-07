5a6cb9ce4fe5fa250c211f5de85369c5fc184e52
Non-Contradiction <lch34677@gmail.com>
2018-07-06 23:08:56 -0400

    Don't use R6 to implement DiffResult.

--- R/DiffResults.R
+++ R/DiffResults.R
@@ -1,65 +1,11 @@
-GradientResult <- R6::R6Class("GradientResult",
-                              public = list(
-                                  name = NULL,
-                                  hair = NULL,
-                                  initialize = function(name = NA, hair = NA) {
-                                      self$name <- name
-                                      self$hair <- hair
-                                      self$greet()
-                                  },
-                                  gradient = function(val) {
-                                      self$hair <- val
-                                  },
-                                  getvalue = function() {
-                                      cat(paste0("Hello, my name is ", self$name, ".\n"))
-                                  },
-                                  getgradient = function() {
-                                      cat(paste0("Hello, my name is ", self$name, ".\n"))
-                                  }
-                              )
-)
+GradientResult <- function(){
 
-JacobianResult <- R6::R6Class("JacobianResult",
-                  public = list(
-                      name = NULL,
-                      hair = NULL,
-                      initialize = function(name = NA, hair = NA) {
-                          self$name <- name
-                          self$hair <- hair
-                          self$greet()
-                      },
-                      jacobian = function(val) {
-                          self$hair <- val
-                      },
-                      getvalue = function() {
-                          cat(paste0("Hello, my name is ", self$name, ".\n"))
-                      },
-                      getjacobian = function() {
-                          cat(paste0("Hello, my name is ", self$name, ".\n"))
-                      }
-                  )
-)
+}
 
-HessianResult <- R6::R6Class("HessianResult",
-                              public = list(
-                                  name = NULL,
-                                  hair = NULL,
-                                  initialize = function(name = NA, hair = NA) {
-                                      self$name <- name
-                                      self$hair <- hair
-                                      self$greet()
-                                  },
-                                  hessian = function(val) {
-                                      self$hair <- val
-                                  },
-                                  getvalue = function() {
-                                      cat(paste0("Hello, my name is ", self$name, ".\n"))
-                                  },
-                                  getgradient = function() {
-                                      cat(paste0("Hello, my name is ", self$name, ".\n"))
-                                  },
-                                  gethessian = function() {
-                                      cat(paste0("Hello, my name is ", self$name, ".\n"))
-                                  }
-                              )
-)
+JacobianResult <- function(){
+
+}
+
+HessianResult <- function(){
+
+}
