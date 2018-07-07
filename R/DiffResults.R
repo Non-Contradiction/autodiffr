72d111de5fea806cdccf41ce2e72404672da45d2
Non-Contradiction <lch34677@gmail.com>
2018-07-07 09:45:23 -0400

    Initial implementation for DiffResults.

--- R/DiffResults.R
+++ R/DiffResults.R
@@ -1,11 +1,21 @@
-GradientResult <- function(){
-
+GradientResult <- function(x){
+    r <- JuliaCall::julia_call("DiffResults.GradientResult", x)
+    makeActiveBinding("value", function() JuliaCall::julia_call("DiffResults.value", r), r)
+    makeActiveBinding("grad", function() JuliaCall::julia_call("DiffResults.gradient", r), r)
+    r
 }
 
-JacobianResult <- function(){
-
+JacobianResult <- function(x){
+    r <- JuliaCall::julia_call("DiffResults.JacobianResult", x)
+    makeActiveBinding("value", function() JuliaCall::julia_call("DiffResults.value", r), r)
+    makeActiveBinding("jacobian", function() JuliaCall::julia_call("DiffResults.jacobian", r), r)
+    r
 }
 
-HessianResult <- function(){
-
+HessianResult <- function(x){
+    r <- JuliaCall::julia_call("DiffResults.HessianResult", x)
+    makeActiveBinding("value", function() JuliaCall::julia_call("DiffResults.value", r), r)
+    makeActiveBinding("grad", function() JuliaCall::julia_call("DiffResults.gradient", r), r)
+    makeActiveBinding("hessian", function() JuliaCall::julia_call("DiffResults.hessian", r), r)
+    r
 }
