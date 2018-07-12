dd9e266cb99f6cdc6214821f08dfd651e80d25d6
Non-Contradiction <lch34677@gmail.com>
2018-07-12 15:07:33 -0400

    Split interface functions.

--- R/zinterface.R
+++ R/zinterface.R
@@ -5,19 +5,6 @@
 #' @param func the target function to calculate gradient, jacobian or hessian.
 #' @param x the input(s) where derivative is (are) taken.
 #' @param mode whether to use forward or reverse mode automatic differentiation.
-#' @param xsize If `x` is not given and `xsize` is given, then the returning functiopn
-#'   will be optimized w.r.t. the input(s) of the same size with `x`.
-#'   Note that if `xsize` is not given, then all the following parameters will be ignored.
-#' @param chunk_size the chunk size to use in forward mode automatic differentiation.
-#'   Note that this parameter is only effective when `mode = "forward"`.
-#' @param use_tape whether or not to use tape for reverse mode automatic differentiation.
-#'   Note that although `use_tape` can greatly improve the performance sometime,
-#'   it can only calculate the derivatives w.r.t a certain branching,
-#'   so it may not give the correct result for functions containing things like `if`.
-#'   And this parameter is only effective when `mode = "reverse"`.
-#' @param compiled whether or not to use compiled tape for reverse mode automatic differentiation.
-#'   Note that tape compiling can take a lot of time and
-#'   this parameter is only effective when `use_tape = TRUE`.
 #' @param ... other arguments passed to the target function `func`.
 #'
 #' @return if `x` is given, then return will be derivatives;
@@ -32,23 +19,15 @@ createInterface <- function(fname = c("grad", "jacobian", "hessian")){
     fname <- match.arg(fname)
     if (fname == "grad") {
         D <- list(forward = multi.forward.grad, reverse = reverse.grad)
-        Config <- list(forward = multi.forward.grad.config, reverse = reverse.grad.config)
-        Tape <- reverse.grad.tape
     }
     if (fname == "jacobian") {
         D <- list(forward = multi.forward.jacobian, reverse = reverse.jacobian)
-        Config <- list(forward = multi.forward.jacobian.config, reverse = reverse.jacobian.config)
-        Tape <- reverse.jacobian.tape
     }
     if (fname == "hessian") {
         D <- list(forward = multi.forward.hessian, reverse = reverse.hessian)
-        Config <- list(forward = multi.forward.hessian.config, reverse = reverse.hessian.config)
-        Tape <- reverse.hessian.tape
     }
 
-    f <- function(func, x = NULL, ..., mode = c("reverse", "forward"),
-                  xsize = x, chunk_size = NULL,
-                  use_tape = FALSE, compiled = FALSE){
+    f <- function(func, x, ..., mode = c("reverse", "forward")){
         ## ad_setup() is not necessary,
         ## unless you want to pass some arguments to it.
         ad_setup()
@@ -74,49 +53,8 @@ createInterface <- function(fname = c("grad", "jacobian", "hessian")){
             }
 
         mode <- match.arg(mode)
-        if (!is.null(x)) {
-            ## when x is given, return result directly,
-            ## and no optimization is possible
-            return(D[[mode]](target, scalar2vector(x)))
-        }
-        ## when x is null, need to return a function
 
-        if (is.null(xsize)) {
-            ## since xsize is not given, no optimization will be carried on
-            return(function(x) D[[mode]](target, scalar2vector(x)))
-        }
-
-        ## xsize is given, further optimization is possible
-        if (mode == "forward") {
-            ## when mode is forward,
-            ## the only possible optimization is construction of Config objects
-
-            ## note that the chunk size cannot be greater than length(x)
-            if (!is.null(chunk_size)) {
-                if (chunk_size > alength(xsize)) {
-                    warning("chunk size cannot be greater than the length of input vector(s),
-                            automatically reduce chunk size.")
-                    chunk_size <- alength(xsize)
-                }
-            }
-
-            config <- Config$forward(target, scalar2vector(xsize), chunk_size = chunk_size)
-            return(function(x) D$forward(target, scalar2vector(x), cfg = config))
-        }
-
-        if (isFALSE(use_tape)) {
-            ## If not use_tape,
-            ## the only possible optimization in rev mode is also construction of Config objects
-            config <- Config$reverse(scalar2vector(xsize))
-            return(function(x) D$reverse(target, scalar2vector(x), cfg = config))
-        }
-
-        ## use_tape,
-        tape <- Tape(target, scalar2vector(xsize))
-        if (isTRUE(compiled)) {
-            tape <- reverse.compile(tape)
-        }
-        return(function(x) D$reverse(tape, scalar2vector(x)))
+        D[[mode]](target, scalar2vector(x)))
     }
 
     f
---
+++ R/zmakeinterface.R
@@ -0,0 +1,139 @@
+#' Create (Optimized) Gradient, Jacobian and Hessian Functions using Automatic Differentiation.
+#'
+#' These functions give (optimized) gradient, jacobian or hessian functions for the target function.
+#'
+#' @param func the target function to calculate gradient, jacobian or hessian.
+#' @param ... other arguments passed to the target function `func`.
+#' @param mode whether to use forward or reverse mode automatic differentiation.
+#' @param x If `x` is given, then the returning functiopn
+#'   will be optimized w.r.t. the input(s) of the same shape with `x`.
+#'   Note that if `x` is not given, then all the following parameters will be ignored.
+#' @param chunk_size the chunk size to use in forward mode automatic differentiation.
+#'   Note that this parameter is only effective when `mode = "forward"`.
+#' @param use_tape whether or not to use tape for reverse mode automatic differentiation.
+#'   Note that although `use_tape` can greatly improve the performance sometime,
+#'   it can only calculate the derivatives w.r.t a certain branching,
+#'   so it may not give the correct result for functions containing things like `if`.
+#'   And this parameter is only effective when `mode = "reverse"`.
+#' @param compiled whether or not to use compiled tape for reverse mode automatic differentiation.
+#'   Note that tape compiling can take a lot of time and
+#'   this parameter is only effective when `use_tape = TRUE`.
+#'
+#' @return if `x` is not given, the returned function will be a general function to calculate derivatives,
+#'  which accepts other arguments to `func` besides `x`;
+#'  if `x` is given, the returned function will be an optimized function speciallised for inputs of the same
+#'  shape with the given `x`.
+#'
+#' @md
+#'
+#' @name autodifffunc
+NULL
+
+funcInterface <- function(fname = c("grad", "jacobian", "hessian")){
+    fname <- match.arg(fname)
+    if (fname == "grad") {
+        D <- list(forward = multi.forward.grad, reverse = reverse.grad)
+        Config <- list(forward = multi.forward.grad.config, reverse = reverse.grad.config)
+        Tape <- reverse.grad.tape
+        interface <- grad
+    }
+    if (fname == "jacobian") {
+        D <- list(forward = multi.forward.jacobian, reverse = reverse.jacobian)
+        Config <- list(forward = multi.forward.jacobian.config, reverse = reverse.jacobian.config)
+        Tape <- reverse.jacobian.tape
+        interface <- jacobian
+    }
+    if (fname == "hessian") {
+        D <- list(forward = multi.forward.hessian, reverse = reverse.hessian)
+        Config <- list(forward = multi.forward.hessian.config, reverse = reverse.hessian.config)
+        Tape <- reverse.hessian.tape
+        interface <- hessian
+    }
+
+    f <- function(func, ..., mode = c("reverse", "forward"),
+                  x = NULL, chunk_size = NULL,
+                  use_tape = FALSE, compiled = FALSE){
+        ## ad_setup() is not necessary,
+        ## unless you want to pass some arguments to it.
+        ad_setup()
+
+        ## construction of target function
+        force(func)
+        dot <- list(...)
+
+        ## when x is null, need to return an unoptimized function
+
+        if (is.null(x)) {
+            return(function(...) do.call(interface, c(list(...), dot)))
+        }
+
+        target0 <- function(...){
+            x <- list(...)
+            do.call(func, c(x, dot))
+        }
+
+        ## in jacobian, the output of the target function should be a vector,
+        ## otherwise a scalar
+
+        target <-
+            if (fname == "jacobian") {
+                function(...) scalar2vector(target0(...))
+            }
+            else {
+                function(...) vector2scalar(target0(...))
+            }
+
+        mode <- match.arg(mode)
+
+        ## x is given, further optimization is possible
+        if (mode == "forward") {
+            ## when mode is forward,
+            ## the only possible optimization is construction of Config objects
+
+            ## note that the chunk size cannot be greater than length(x)
+            if (!is.null(chunk_size)) {
+                if (chunk_size > alength(x)) {
+                    warning("chunk size cannot be greater than the length of input vector(s),
+                            automatically reduce chunk size.")
+                    chunk_size <- alength(x)
+                }
+            }
+
+            config <- Config$forward(target, scalar2vector(x), chunk_size = chunk_size)
+            return(function(x) D$forward(target, scalar2vector(x), cfg = config))
+        }
+
+        if (isFALSE(use_tape)) {
+            ## If not use_tape,
+            ## the only possible optimization in rev mode is also construction of Config objects
+            config <- Config$reverse(scalar2vector(x))
+            return(function(x) D$reverse(target, scalar2vector(x), cfg = config))
+        }
+
+        ## use_tape,
+        tape <- Tape(target, scalar2vector(x))
+        if (isTRUE(compiled)) {
+            tape <- reverse.compile(tape)
+        }
+        return(function(x) D$reverse(tape, scalar2vector(x)))
+    }
+
+    f
+}
+
+#' @rdname autodifffunc
+#' @export
+makeJacobianFunc <- funcInterface("jacobian")
+
+#' @rdname autodifffunc
+#' @export
+makeHessianFunc <- funcInterface("hessian")
+
+#' @rdname autodifffunc
+#' @export
+makeGradFunc <- funcInterface("grad")
+
+#' @rdname autodiff
+#' @export
+makeDerivFunc <- makeGradFunc
+
